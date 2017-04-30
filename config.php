405e37f26f7faf3715b7b65cdf2faed41ee29a5d
misunderstood173 <marius.vladu@ulbsibiu.ro>
2017-04-30 14:05:01 +0300

    config file for db connection
    in directory 2 levels up

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('DBHOST')) define('DBHOST', 'localhost');
+if (!defined('DBPORT')) define('DBPORT', '8080');
+if (!defined('DBNAME')) define('DBNAME', 'warehouse');
+if (!defined('DBUSER')) define('DBUSER', 'root');
+if (!defined('DBPASS')) define('DBPASS', 'password');
+?>
