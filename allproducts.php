7c777d14de3d2fcbf456ff76046c158feb2f8143
misunderstood173 <marius.vladu@ulbsibiu.ro>
2017-05-09 15:12:05 +0300

    pages added

--- allproducts.php
+++ allproducts.php
@@ -9,6 +9,10 @@
   <h3>All products in warehouse</h3>
 <?php
 
+$results_per_page = 10;
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+$start_from = ($page-1) * $results_per_page;
+
 echo "<table>";
 echo "<tr>
       <th>Product Name</th>
@@ -21,13 +25,14 @@ try {
 	require "connection.php";
 
   $stmt = $conn->prepare(
-    "SELECT products.ID, products.Name, countries.country_name, products.Quantity,
+    'SELECT products.ID, products.Name, countries.country_name, products.Quantity,
     units_of_measure.Abbreviation, units_of_measure.unit_name ,employees.Full_name, products.Last_time_modified
     FROM products
     JOIN countries on products.Country_ID = countries.ID
     JOIN units_of_measure on products.Unit_of_measure_ID = units_of_measure.ID
     JOIN employees on products.Last_modified_by_employee_ID = employees.ID
-		ORDER BY products.ID ASC"
+		ORDER BY products.ID ASC
+		LIMIT ' . $start_from . ', ' . $results_per_page
     );
   $stmt->execute();
 
@@ -45,6 +50,17 @@ try {
 	}
 
 	echo '</table>';
+
+	$stmt = $conn->prepare("SELECT COUNT(ID) AS total FROM products");
+	$stmt->execute();
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	$total_pages = ceil($row["total"] / $results_per_page);
+
+	for ($i=1; $i<=$total_pages; $i++) {
+	            echo "<a href='/warehousestocks/allproducts.php?page=" . $i . "'";
+	            if ($i==$page)  echo " class='currentPage'";
+	            echo ">".$i."</a> ";
+	};
 
 } catch (PDOException  $e) {
   echo "Connection failed: " . $e->getMessage();
